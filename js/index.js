caa6b97c6008ac632d36b9cef7e0c84baea6c2d8
Rafael Santos Carvalho <88905368+Oieusouopi@users.noreply.github.com>
2025-06-08 22:20:57 +0000

    modificacao

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 import Apoio from "./model/Apoio.js";
 
+const apiKey = "AIzaSyB8L8PfjgbIApcO6BdEVXptWBjRp0WnZBM";
+let map;
+let directionsService;
+let directionsRenderer;
+let rotaUnica;
+
 function onInit() {
     fetch('./utils/dados.json')
         .then(response => response.json())
@@ -11,8 +17,46 @@ function onInit() {
             }
         })
         .catch(error => console.error('Erro ao carregar os dados:', error));
+
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                addUserLocationAdvancedMarker(position);
+            }
+        ), (error) => {
+            console.error(`Erro: ${error.message}`);
+        }
+    }
 }
 
+function addUserLocationAdvancedMarker(position) {
+    if (typeof google.maps.marker !== 'undefined') {
+        const marker = new google.maps.marker.AdvancedMarkerElement({
+            position: {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            },
+            map: map,
+            title: "Sua localização",
+            content: createCustomUserMarker()
+        });
+        return marker;
+    } else {
+        return addUserLocationMarker(position);
+    }
+}
+
+
+function createCustomUserMarker() {
+    const marker = document.createElement('div');
+    marker.style.width = '20px';
+    marker.style.height = '20px';
+    marker.style.backgroundColor = 'blue';
+    marker.style.border = '3px solid white';
+    marker.style.borderRadius = '50%';
+    marker.style.boxShadow = '0 0 6px rgba(0,0,0,0.5)';
+    return marker;
+}
 
 function setCards(card) {
     let apoio = new Apoio(card);
@@ -58,20 +102,103 @@ function setCards(card) {
     container.addEventListener('click', () => {
         const map = document.getElementById('map');
         map.setAttribute('center', `${apoio.local.x},${apoio.local.y}`);
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    computeRoute(position, apoio.local)
+                }
+            ), (error) => {
+                console.error(`Erro: ${error.message}`);
+            }
+        }
     });
+}
+
+function computeRoute(currentPosition, destination) {
+    fetch(`https://routes.googleapis.com/directions/v2:computeRoutes`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Goog-Api-Key': apiKey,
+            'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline'
+        },
+        body: JSON.stringify({
+            origin: {
+                location: {
+                    latLng: {
+                        latitude: currentPosition.coords.latitude,
+                        longitude: currentPosition.coords.longitude
+                    }
+                }
+            },
+            destination: {
+                location: {
+                    latLng: {
+                        latitude: destination.x,
+                        longitude: destination.y
+                    }
+                }
+            },
+            travelMode: 'DRIVE'
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            
+            if (rotaUnica) {
+                rotaUnica.setMap(null);
+            }
+
+            const rota = data.routes[0];
+            drawPolyline(rota.polyline.encodedPolyline);
+        })
+        .catch(error => console.error('Erro:', error));
+}
+
+function drawPolyline(encoded) {
+    const decodedPath = google.maps.geometry.encoding.decodePath(encoded);
+
+    rotaUnica = new google.maps.Polyline({
+        path: decodedPath,
+        geodesic: true,
+        strokeColor: "#4285F4",
+        strokeOpacity: 1.0,
+        strokeWeight: 4
+    });
+
+    rotaUnica.setMap(map);
 }
 
 function setAdvancedMapMarker(card) {
     const apoio = new Apoio(card);
-    const map = document.getElementById('map');
 
-    const gMapAdvancedMarker = document.createElement('gmp-advanced-marker');
-    gMapAdvancedMarker.setAttribute('position', `${apoio.local.x},${apoio.local.y}`);
-    map.appendChild(gMapAdvancedMarker);
+    const marker = new google.maps.Marker({
+        position: { lat: parseFloat(apoio.local.x), lng: parseFloat(apoio.local.y) },
+        map: map,
+        title: card.nome
+    });
+
+    return marker;
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: { lat: -19.918914794921875, lng: -43.938743591308594 },
+        mapId: "DEMO_MAP_ID"
+    });
+
+    directionsService = new google.maps.DirectionsService();
+    directionsRenderer = new google.maps.DirectionsRenderer({
+        strokeColor: "#4285F4",
+        strokeOpacity: 1.0,
+        strokeWeight: 4
+    });
+    directionsRenderer.setMap(map);
 }
 
 
-
 document.addEventListener('DOMContentLoaded', function () {
+    initMap();
     onInit();
 });
